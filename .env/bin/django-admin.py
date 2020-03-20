ef642310f70e8f058f9f5b5da5cc61cf5e7008d7
dennischen123 <dennischen123@gmail.com>
2020-03-20 00:46:11 -0700

    boilerplate

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Dennis/coding/GA-SEI/fundamentals/django/BudgetFusion/.env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
