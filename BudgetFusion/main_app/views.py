c9d2bd2fd8857a9fb8123e0189e2a620bfc6e538
dennischen123 <dennischen123@gmail.com>
2020-03-23 22:58:14 -0700

    cleaned up views.py

--- BudgetFusion/main_app/views.py
+++ BudgetFusion/main_app/views.py
@@ -8,14 +8,13 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 
-# Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
-# Users -> Budget -> Category -> expense
+# api views
 def api(request, user_id, budget_id):
     budgets = Budget.objects.filter(user_id=user_id)
     budget = Budget.objects.get(id=budget_id)
@@ -40,13 +39,13 @@ def reports(request, user_id):
 
 # crud functions for Budget ###
 ###########################################################
-#function for index(all budget)
+
 def budget_index(request):
     budgets = Budget.objects.all()
     return render(request, 'budgets/index.html', { 'budgets' : budgets})
 
 
-#function for detail(single budget)
+
 def budget_detail(request, budget_id):
     budgets = Budget.objects.all()
     budget = Budget.objects.get(id=budget_id)
@@ -62,7 +61,7 @@ def budget_detail(request, budget_id):
         })
 
 
-#function for create
+
 def budget_create(request):
     if request.method == 'POST':
         budget_form = BudgetForm(request.POST)
@@ -75,7 +74,7 @@ def budget_create(request):
         budget_form = BudgetForm()
     return render(request, 'budgets/budget_form.html', {'budget_form' : budget_form})
 
-#function for update
+
 def budget_update(request, budget_id):
     budget = Budget.objects.get(id=budget_id)
 
@@ -88,26 +87,25 @@ def budget_update(request, budget_id):
         budget_form = BudgetForm(instance=budget)
     return render(request, 'budgets/budget_form.html', { 'budget_form' : budget_form })
 
-#function for delete
+
 def budget_delete(request, budget_id):
     return Budget.objects.get(id=budget_id).delete() and redirect('budget_index')
 
 
     # crud functions for Category ##
 ###########################################################
-#function for index(all Category)
+
 def category_index(request, budget_id):
     budget = Budget.objects.get(id=budget_id)
     categories = Category.objects.filter(budget_id=budget_id)
     return render(request, 'categories/category_list.html', { 'categories': categories, 'budget' : budget, 'budget_id' : budget.id })
 
     
-# function for detail(single Category)
+
 def category_detail(request, budget_id, category_id):
     pass
 
 
-# function for create
 def category_create(request, budget_id):
     if request.method == 'POST':
         form = CategoryForm(request.POST)
@@ -116,12 +114,11 @@ def category_create(request, budget_id):
             category.budget_id = budget_id
             category.save()
         return redirect('category_index', budget_id)
-            # return redirect('category_index', budget_id)
     else:
         form = CategoryForm()
     return render(request, 'categories/category_form.html', { 'form' : form})
 
-# function for update
+
 def category_update(request, budget_id, category_id):
     category = Category.objects.get(id=category_id)
     if request.method == 'POST':
@@ -133,27 +130,25 @@ def category_update(request, budget_id, category_id):
         form = CategoryForm(instance=category)
     return render(request, 'categories/category_form.html', { 'form' : form})
 
-# function for delete
+
 def category_delete(request, budget_id, category_id):
     return Category.objects.get(id=category_id).delete() and redirect('category_index', budget_id)
 
 
     # crud functions for Expense ##
 ###########################################################
-#function for index(all Expense)
 
-#index all expense for a category of a budget
+
 def expense_index(request, budget_id, category_id):
     categories = Category.objects.filter(budget_id=budget_id)
     expenses = Expense.objects.filter(category_id=category_id)
     return render(request, 'categories/category_details.html', { 'expenses' : expenses,'categories' : categories, 'category_id' :category_id, 'budget_id' : budget_id})
 
 
-#function for detail(single Expense)
 def expense_detail(request, budget_id, category_id, expense_id):
     pass
 
-#function for create
+
 def expense_create(request, budget_id, category_id):
     if request.method == 'POST':
         form = ExpenseForm(request.POST)
@@ -165,7 +160,6 @@ def expense_create(request, budget_id, category_id):
     return render(request, 'expenses/expense_form.html', { 'form': form})
             
 
-#function for update
 def expense_update(request, budget_id, category_id, expense_id):
     expense = Expense.objects.get(id=expense_id)
     if request.method == 'POST':
@@ -178,8 +172,6 @@ def expense_update(request, budget_id, category_id, expense_id):
     return render(request, 'expenses/expense_form.html', { 'form': form})
 
 
-
-#function for delete
 def expense_delete(request, budget_id, category_id, expense_id):
     return Expense.objects.get(id=expense_id).delete() and redirect('expense_index', budget_id, category_id)
 
