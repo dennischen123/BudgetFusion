118f7bacf8d56c859efe9b9abecf48ebef65638c
dennischen123 <dennischen123@gmail.com>
2020-03-24 13:49:33 -0700

    Config app for heroku deployment

--- BudgetFusion/BudgetFusion/settings.py
+++ BudgetFusion/BudgetFusion/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -121,3 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
